6bd0babf7ead3f3a8c774a717e969d09bd182c7c
Jordan23 <marvincloser@gmail.com>
2022-07-01 22:42:47 -0600

    añadi a app.js la variable de icon-awesome

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import './assets/css/App.css'
+import './assets/css/App.css';
+import Ul from './components/IconAwesome';
 import NavBar from './components/NavBar';
 import WeatherPanel from './components/WeatherPanel';
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
+      <Ul />
+      <Header />
       <NavBar />
       <WeatherPanel />
     </div>
