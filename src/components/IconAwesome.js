b4ad4a769988748dca99f7046a4461c1988feaa3
Jordan23 <marvincloser@gmail.com>
2022-07-01 22:37:38 -0600

    Subir carpeta icon

---
+++ src/components/IconAwesome.js
@@ -0,0 +1,15 @@
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const Ul = () => {
+  
+    return (
+        <a className="Icon-Awesome" href="https://github.com/Jorda23/app.OpenWeather.git">
+            <FontAwesomeIcon icon={faGithub} />
+        </a>
+    );
+  };
+  
+  export default Ul;
+  
+
